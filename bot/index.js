917a5edd7aa0e5dd9cfb4bf94ddac1919e341943
Muhammad <119015633+MuhammadElsoukkary1@users.noreply.github.com>
2025-05-15 14:10:34 -0400

    Remove previously tracked files now ignored by .gitignore

---
+++ bot/index.js
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import { Client } from 'discord.js';
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.DirectMessages
+    ],
+});
+
+client.login(process.env.DISCORD_TOKEN);
